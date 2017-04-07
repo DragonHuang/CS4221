a8abd635bc2654dd32dcfab84089d251961515e9
Weilong <weilong@25-13.priv29.nus.edu.sg>
2017-04-07 15:55:19 +0800

    update for server

--- cs4221/settings.py
+++ cs4221/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$=q()!)+(lxbrh1l21-bn+dadq^8kr0)_^f_sf0h54ni_yjbdc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*.poi.life']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*.poi.life', u'ddl.poi.life']
 
 
 # Application definition
